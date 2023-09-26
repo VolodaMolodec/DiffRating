03f179d720b6a62e7783548ac3de1f08e503cb97
VolodaMolodec <vovanobritano@gmail.com>
2023-09-26 11:13:29 +0500

    SelectionSort

---
+++ DifficultyRating/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DifficultyRating
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ElementsInputTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SelectionSortOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 394);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(101, 44);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Начать тест";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выборочная сортировка";
+            // 
+            // ElementsInputTextBox
+            // 
+            this.ElementsInputTextBox.Location = new System.Drawing.Point(144, 410);
+            this.ElementsInputTextBox.Name = "ElementsInputTextBox";
+            this.ElementsInputTextBox.Size = new System.Drawing.Size(69, 20);
+            this.ElementsInputTextBox.TabIndex = 2;
+            this.ElementsInputTextBox.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Количество элементов";
+            // 
+            // SelectionSortOutput
+            // 
+            this.SelectionSortOutput.Location = new System.Drawing.Point(328, 410);
+            this.SelectionSortOutput.Name = "SelectionSortOutput";
+            this.SelectionSortOutput.ReadOnly = true;
+            this.SelectionSortOutput.Size = new System.Drawing.Size(69, 20);
+            this.SelectionSortOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SelectionSortOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ElementsInputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ElementsInputTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SelectionSortOutput;
+    }
+}
+
---
+++ DifficultyRating/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace DifficultyRating
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class DifficulityRate
+        {
+            public DifficulityRate()
+            {
+                operationsCount = 0;
+                totalTime = 0;
+            }
+            public int operationsCount { get; set; }
+            public int totalTime { get; set; }
+        }
+
+        private DifficulityRate SelectionSort(int[] _array)
+        {
+            DifficulityRate diff = new DifficulityRate();
+            int[] arr = new int[_array.Length];
+            _array.CopyTo(arr, 0);
+            int maxId = 0, lastId = arr.Count() - 1;
+            while(lastId != 0)
+            {
+                for (int i = 0; i <= lastId; i++)
+                {
+                    if (arr[i] > arr[maxId])
+                        maxId = i;
+                    diff.operationsCount++;
+                }
+                (arr[maxId], arr[lastId]) = (arr[lastId], arr[maxId]);
+                maxId = 0;
+                lastId--;
+            }
+            return diff;
+        }
+
+        private void DrawGraph()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(ElementsInputTextBox.Text);
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                arr[i] = rnd.Next() % 100;
+            DifficulityRate SelectionDiff = SelectionSort(arr);
+            SelectionSortOutput.Text = SelectionDiff.operationsCount.ToString();
+        }
+    }
+}
