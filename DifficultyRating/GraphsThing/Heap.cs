4631ec9d3003812b455da1e0a4b7bd6543eb92c8
VolodaMolodec <vovanobritano@gmail.com>
2023-11-25 00:02:51 +0500

    worked on heap

--- DifficultyRating/GraphsThing/Heap.cs
+++ DifficultyRating/GraphsThing/Heap.cs
@@ -9,19 +9,76 @@ namespace DifficultyRating.GraphsThing
 {
     public partial class GraphsForm : Form
     {
-        class Vertex
-        {
-            int value;
-            Vertex lefVert;
-            Vertex rightVert;
-        }
         class Heap
         {
-            private Vertex topVert;
-
-            public Vertex getTop()
+            class Vertex
             {
-                Vertex retVert = topVert;
+                public int value;
+                public Vertex lefVert;
+                public Vertex rightVert;
+                public Vertex()
+                {
+                    value = -1;
+                    lefVert = null;
+                    rightVert = null;
+                }
+                public void Init(int val)
+                {
+                    value = val;
+                    lefVert = new Vertex();
+                    rightVert = new Vertex();
+                }
+            }
+
+            private Vertex topVert = new Vertex();
+
+            public int getTop()
+            {
+                //if (topVert.value == -1)
+                //    return -1;
+                //int res = topVert.value;
+                //if (topVert.lefVert.value == -1) topVert = topVert.rightVert;
+                //else if (topVert.rightVert.value == -1) topVert = topVert.lefVert;
+                //else if(topVert.lefVert.value > topVert.rightVert.value)
+                //{
+
+                //}
+              
+                //return res;
+            }
+
+            public void Add(int val)    //Добавление значения в кучу
+            {
+                List<Vertex> queue = new List<Vertex>   
+                {
+                    topVert
+                };
+                while(queue.Count != 0)
+                {
+                    Vertex currVert = queue[0];
+                    if (currVert.value == -1)
+                    {
+                        currVert.Init(val);
+                        break;
+                    }
+                    else if(currVert.value < val) 
+                    {
+                        Vertex newVert = new Vertex();  //Сложная схема из-за C#. Если кратко, то создаём новый участок памяти, передаём ему все значения текущей области памяти
+                        newVert.Init(currVert.value);   //А текущей области памяти передаём новые значения
+                        newVert.lefVert = currVert.lefVert;
+                        newVert.rightVert = currVert.rightVert;
+                        currVert.lefVert = newVert;
+                        currVert.rightVert = new Vertex();
+                        currVert.value = val;
+                        break;
+                    }
+                    else
+                    {
+                        queue.Add(currVert.lefVert);
+                        queue.Add(currVert.rightVert);
+                    }
+                    queue.RemoveAt(0);      
+                }
             }
         }
     }
